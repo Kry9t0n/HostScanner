bc1a8fb05eb6543f5c1bfabb6f596cb5da9518af
w4r10ck <51532709+Kry9t0n@users.noreply.github.com>
2020-02-23 21:30:35 +0100

    Add files via upload

--- scanner.py
+++ scanner.py
@@ -1,5 +1,7 @@
 import socket
 import os
+import sys
+from orca.messages import percentage
 
 class Scanner:
     def __init__(self, PORTRANGE=100, IPFORMAT="192.168.0.XXX", STARTHOST=0):
@@ -7,19 +9,26 @@ class Scanner:
         self.sock = socket.socket()
         self.IPFORMAT = IPFORMAT
         self.STARTHOST = STARTHOST
+        self.aboveLineLength = 0
         self.hostDict = dict()
     
     def scan(self):
         global ip
+        print("Start scanning...")
+        print("Scanning progress:")
         for i in range(self.STARTHOST, self.PORTRANGE):
+            #status = "Scanning: "
             ip = self._parseIPFormat(i)
-            print(ip)
+            self.progress("Scanning", i, self.STARTHOST+self.PORTRANGE, 50, 20)
+            print(ip, sep='', end='\b'*(len(ip)+self.aboveLineLength), flush=True)
             #os.system('clear')
             res = True if os.system('ping -c 1 ' + ip + ' >/dev/null') is 0 else False
             if(res):
                 self.hostDict[ip] = 'alive'
             else:
                 self.hostDict[ip] = 'down'
+        print("\n")
+        print("Done...")
             
     def _parseIPFormat(self, IPPOST):
         if('XXX' in self.IPFORMAT):
@@ -28,9 +37,24 @@ class Scanner:
             raise Exception("Ip Format Error!")
         
     def printRES(self):
+        print("Stats:")
         for i in self.hostDict:
             print(">>> HOST " + i + " " + "STATUS: "+self.hostDict.get(i))
         print()
+    
+    def progress(self, name, start, end, tokens = 50, width = 20):
+        percentage = float(start)/end
+        arrow = "-"*int(round(percentage*tokens)-1)+">"
+        spaces = ' '*(tokens-len(arrow))
+        sys.stdout.write("\r{0: <{1}} : [{2}]{3}%\n".format(name, width, arrow + spaces, int(round(percentage*100))))
+        self.aboveLineLength = len("\r{0: <{1}} : [{2}]{3}%\n".format(name, width, arrow + spaces, int(round(percentage*100))))
+        sys.stdout.flush()
+        if start == end:
+            sys.stdout.write('\n\n')
         
             
             
+if __name__ == '__main__':
+    sc = Scanner(20, '192.168.0.XXX', 0)
+    sc.scan()
+    sc.printRES()
